bd57b472fb532638e08a892b6391e2b868b3567d
TamariIv <taivgi@g.jct.ac.il>
2023-06-25 23:50:21 +0300

    complete main

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,10 @@
 #include <sstream>
 #include "CityExplorer.h"
 
-const std::string DEFAULT_DATA_PATH = "C:\\Users\\tamar\\excellenteam\\CPP\\Task05\\Task05\\Task05\\resources\\data.txt";
+const std::string DEFAULT_DATA_PATH = "data.txt";
 
 void initExplorer(CityExplorer& ce);
+void printResults(const std::multimap<Coordinate, std::string>& cities);
 
 int main() {
 
@@ -16,24 +17,46 @@ int main() {
 		std::string command = "";		
 		float radius = 0;
 		int norm = 0;	
+
 		CityExplorer explorer;
 		initExplorer(explorer);
-		std::map<Coordinate, std::string> closeCities = {};
+		std::multimap<Coordinate, std::string> closeCities = {};
 		
 		std::cout << "Please enter selected city name (with line break after it):\n";
-		std::cin >> command;
+		std::getline(std::cin, command);
 
 		while (command != "0") {
-			std::cout << "Please enter the wanted radius\n";
+			while (!explorer.cityExists(command)) {
+				std::cout << "City doesn't exist.\n";
+				std::cout << "Please enter selected city name (with line break after it):\n";
+				std::getline(std::cin, command);
+			}
+			std::cout << "Please enter the wanted radius:\n";
 			std::cin >> radius;
-			std::cout << "Please enter the wanted norm (0 – L2, Euclidean distance, 1 – Linf, Chebyshev distance, 2 – L1, Manhattan distance):\n";
+			while (radius < 0) {
+				std::cout << "Invalid radius.\n";
+				std::cout << "Please enter the wanted radius:\n";
+				std::cin >> radius;
+			}
+			std::cout << "Please enter the wanted norm (0 - L2, Euclidean distance, 1 - Linf, Chebyshev distance, 2 - L1, Manhattan distance):\n";
 			std::cin >> norm;
+			while (norm < 0 || norm > 2) {
+				std::cout << "Invalid norm number.\n";
+				std::cout << "Please enter the wanted norm (0 - L2, Euclidean distance, 1 - Linf, Chebyshev distance, 2 - L1, Manhattan distance):\n";
+				std::cin >> norm;
+			}
 			closeCities = explorer.explore(command, radius, norm);
+			std::cout << "Number of northern cities: " << explorer.northernCities(command) << std::endl;
+			std::cout << "Total number of cities: " << explorer.getCityCount() << std::endl;
+			printResults(closeCities);
+			std::cout << "Please enter selected city name (with line break after it):\n";
+			std::getline(std::cin, command);
 		}
 	}
-	catch (const std::logic_error& e)
+	catch (const std::exception& e)
 	{
 		std::cout << "Caught exception: " << e.what() << std::endl;
+		return 1;
 	}
 
 	std::cout << "Bye\n";
@@ -67,5 +90,13 @@ void initExplorer(CityExplorer& ce)
 			}
 		}
 	}
-	std::cout << "hi\n";
+}
+
+void printResults(const std::multimap<Coordinate, std::string>& cities)
+{
+	auto it = cities.cbegin();
+	while (it != cities.cend()) {
+		std::cout << (*it).second << std::endl;
+		it++;
+	}
 }
